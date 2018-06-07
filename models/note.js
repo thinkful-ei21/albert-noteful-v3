3ab885d5d4866aefe1eb5bf43bce3f331882085f
Albert Sare <thesundaymailman@gmail.com>
2018-06-07 09:11:49 -0700

    added folderSchema and is now referenced by notesSchema; ready to update folder endpoints

--- models/note.js
+++ models/note.js
@@ -7,26 +7,24 @@ const noteSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  content: String,
-  // createdAt: {
-  //   type: Date,
-  //   default: new Date(Date.now())
-  // },
-  // updatedAt: {
-  //   type: Date,
-  //   default: new Date(Date.now())
-  // }
+  content: {
+    type: String
+  },
+  folderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Folder'
+  }
 });
 
-// Add `createdAt` and `updatedAt` fields
+// Add 'createdAt' and 'updatedAt' fields
 noteSchema.set('timestamps', true);
 
 // Customize the mongoose document instance being returned
 noteSchema.set('toObject', {
-  virtuals: true,     // include built-in virtual `id`
-  versionKey: false,  // remove `__v` version key
+  virtuals: true, // include built-in virtual 'id'
+  versionKey: false, // remove '__v' version key
   transform: (doc, ret) => {
-    delete ret._id; // delete `_id`
+    delete ret._id; // delete '_id'
   }
 });
 
--- utils/seed-database.js
+++ utils/seed-database.js
@@ -3,17 +3,32 @@
 const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config.js');
+
+const Folder = require('../models/folder.js');
 const Note = require('../models/note.js');
 
+const seedFolders = require('../db/seed/folders.json');
 const seedNotes = require('../db/seed/notes.json');
 
 mongoose.connect(MONGODB_URI)
-  .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
-  .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+  .then(() => {
+    console.info('Dropping Database');
+    return mongoose.connection.db.dropDatabase();
   })
-  .then(() => mongoose.disconnect())
+  .then(() => {
+    console.info('Seeding Database');
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes(),
+    ]);
+  })
+  .then(() => {
+    console.info('Disconnecting');
+    return mongoose.disconnect();
+  })
   .catch(err => {
+    console.error(`ERROR: ${err.message}`);
     console.error(err);
+    db.disconnect();
   });
